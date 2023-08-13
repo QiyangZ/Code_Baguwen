a9c5528243c654594c97e190f8dc33d15a4aa688
QiyangZ <zongqiyang997@gmail.com>
2023-08-13 14:15:19 +0200

    max profit plus

---
+++ 8_maxProfit_II.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+//greedy algorithm:
+// class Solution{
+//     public:
+//     int maxProfit(std::vector<int>& prices) {
+//         if(prices.empty() || prices.size() == 1) return 0;
+//         int maxProf = 0;
+//         for(int i = 1; i<prices.size(); i++){
+//             if(prices[i] > prices[i-1]){
+//                 maxProf += prices[i] - prices[i-1];
+//             }
+//         }
+//         return maxProf;
+
+//     }
+// };
+
+
+//dynamic programming
+class Solution{
+    public:
+    int maxProfit(std::vector<int>& prices){
+        if(prices.empty() || prices.size() == 1) return 0;
+        int n = prices.size();
+        int dp[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i<n; i++){
+            dp[i][0] = std::max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = std::max(dp[i-1][0] - prices[i], dp[i-1][1]);
+        }
+        return dp[n-1][0];
+    }
+};
+
+int main(){
+    std::vector<int> a = {1,2,3,4,5,6,7};
+    Solution sl;
+    int prof = sl.maxProfit(a);
+    std::cout<<prof<<std::endl;
+    return 0;
+}
