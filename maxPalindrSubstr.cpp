b8b0764b43e2be02798530d1a79890f7f1ace892
QiyangZ <zongqiyang997@gmail.com>
2023-08-22 23:08:38 +0200

    center idea

--- maxPalindrSubstr.cpp
+++ maxPalindrSubstr.cpp
@@ -5,6 +5,7 @@
 #include<cmath>
 #include<algorithm>
 #include<unordered_map>
+#include<utility>
 using namespace std;
 
 
@@ -54,52 +55,89 @@ using namespace std;
 //     }
 // };
 
-// return the string
+// return the string cannot deal with "aaaa"
+// class Solution{
+//     public:
+//     string longestPalindrome(string s){
+//         if(s.length() <= 1) return s;
+//         unordered_map<string,int> strAndLen;
+//         string answerSubSt;
+//         int n = s.length();
+//         for(int i=1; i<=n/2; i++){
+//             int left = i-1;
+//             int right =i+1;
+//             int count = 0;
+//             if(s[left] == s[i] && s[right] != s[i]){
+//                 right = i;
+//                 while(left>=0 && right<=n-1 && s[left] == s[right]){
+//                     left--;
+//                     right++;
+//                     count += 2;
+//                 }
+//             }else if(s[right] == s[i] && s[left] != s[i]){
+//                 left = i;
+//                 while(left>=0 && right<=n-1 && s[left] == s[right]){
+//                     left--;
+//                     right++;
+//                     count += 2; 
+//                 }
+//             }else{
+//                  while(left>=0 && right<=n-1 && s[left] == s[right]){
+//                     left--;
+//                     right++;
+//                     count += 2;
+//                 }
+//                 count +=1;
+//             }
+
+//             strAndLen[s.substr(left + 1, right -left-1)] = count;
+        
+//         }
+
+//         int maxLen = 0;
+//         for(auto pair: strAndLen){
+//             if(pair.second > maxLen ){
+//                 maxLen = pair.second;
+//                 answerSubSt = pair.first;
+//             }
+//         }
+//         return answerSubSt;
+//     }
+// };
+
 class Solution{
     public:
-    string longestPalindrome(string s){
-        unordered_map<string,int> strAndLen;
-        string answerSubSt;
+    pair<int,int> getIndex(string s, int left, int right){
         int n = s.length();
-        for(int i=1; i<=n/2; i++){
-            int left = i-1;
-            int right =i+1;
-            int count = 0;
-            if(s[left] == s[i] && s[right] != s[i]){
-                right = i;
-                while(left>=0 && right<=n-1 && s[left] == s[right]){
-                    left--;
-                    right++;
-                    count += 2;
-                }
-            }else if(s[right] == s[i] && s[left] != s[i]){
-                left = i;
-                while(left>=0 && right<=n-1 && s[left] == s[right]){
-                    left--;
-                    right++;
-                    count += 2; 
-                }
-            }else{
-                 while(left>=0 && right<=n-1 && s[left] == s[right]){
-                    left--;
-                    right++;
-                    count += 2;
-                }
-                count +=1;
-            }
-
-            strAndLen[s.substr(left + 1, right -left-1)] = count;
+        while(left>=0 && right<n && s[left]==s[right]){
+            left--;
+            right++;
+        }
+        return {left + 1,right -1 };
         
-        }
+    }
+    
+    string longestPalindrome(string s){
+        int n = s.length();
+        int start=0, end=0;
+        if(n<2) return s;
+        for(int i =0; i<n; i++){
+            auto [left1,right1] = getIndex(s,i,i);
+            auto [left2,right2] = getIndex(s,i,i + 1);
 
-        int maxLen = 0;
-        for(auto pair: strAndLen){
-            if(pair.second > maxLen ){
-                maxLen = pair.second;
-                answerSubSt = pair.first;
+            if (right1-left1 > end - start){
+                start = left1;
+                end = right1;
+            }
+
+            if(right2-left2 > end - start){
+                start = left2;
+                end = right2;
             }
         }
-        return answerSubSt;
+
+        return s.substr(start, end - start + 1);
+
     }
 };
 
