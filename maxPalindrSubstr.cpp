4661fb3abc45f05c5198542a2e8ab955702433ac
QiyangZ <zongqiyang997@gmail.com>
2023-08-22 19:32:45 +0200

    maxP for Ubuntu to test

--- maxPalindrSubstr.cpp
+++ maxPalindrSubstr.cpp
@@ -4,12 +4,62 @@
 #include<vector>
 #include<cmath>
 #include<algorithm>
+#include<unordered_map>
 using namespace std;
 
+
+// return the lenth
+// class Solution{
+//     public:
+//     int longestPalindrome(string s){
+//         vector<int> quanti;
+//         int n = s.length();
+//         for(int i=1; i<=n/2; i++){
+//             int left = i-1;
+//             int right =i+1;
+//             int count = 0;
+//             if(s[left] == s[i] && s[right] != s[i]){
+//                 right = i;
+//                 while(left>=0 && right<=n-1){
+//                     if(s[left] == s[right]){
+//                     left--;
+//                     right++;
+//                     count += 2;
+//                     }
+//                 }
+//             }else if(s[right] == s[i] && s[left] != s[i]){
+//                 left = i;
+//                 while(left>=0 && right<=n-1){
+//                     if(s[left] == s[right]){
+//                     left--;
+//                     right++;
+//                     count += 2;
+//                     }
+//                 }
+//             }else{
+//                  while(left>=0 && right<=n-1){
+//                     if(s[left] == s[right]){
+//                     left--;
+//                     right++;
+//                     count += 2;
+//                     }
+//                 }
+//                 count +=1;
+//             }
+
+//             quanti.push_back(count);
+        
+//         }
+//         return *max_element(quanti.begin(), quanti.end());
+//     }
+// };
+
+// return the string
 class Solution{
     public:
-    int longestPalindrome(string s){
-        vector<int> quanti;
+    string longestPalindrome(string s){
+        unordered_map<string,int> strAndLen;
+        string answerSubSt;
         int n = s.length();
         for(int i=1; i<=n/2; i++){
             int left = i-1;
@@ -44,37 +94,26 @@ class Solution{
                 count +=1;
             }
 
-            quanti.push_back(count);
+            strAndLen[s.substr(left + 1, right -left-1)] = count;
         
         }
-        return *max_element(quanti.begin(), quanti.end());
+
+        int maxLen = 0;
+        for(auto pair: strAndLen){
+            if(pair.second > maxLen ){
+                maxLen = pair.second;
+                answerSubSt = pair.first;
+            }
+        }
+        return answerSubSt;
     }
 };
 
-// class Solution {
-// public:
-//     int longestPalindrome(string s) {
-//         int n = s.length();
-//         int maxLen = 0;
-
-//         for (int i = 0; i < n/2; i++) {
-//             int left = i;
-//             int right = i;
-//             while (left >= 0 && right < n && s[left] == s[right]) {
-//                 maxLen = max(maxLen, right - left + 1);
-//                 left--;
-//                 right++;
-//             }
-//         }
-
-//         return maxLen;
-//     }
-// };
 
 int main(){
     string s = "babad";
     Solution sol;
-    int longti = sol.longestPalindrome(s);
-    cout<<"answer is: "<< longti<<endl;
+    string longti = sol.longestPalindrome(s);
+    cout<<"answer is: "<< longti <<endl;
     return 0;
 }
